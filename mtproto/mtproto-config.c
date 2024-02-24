b20220e5f63e7f71d9122d3b2d96a04eea18a2e4
aphrco <52441136+aphrco@users.noreply.github.com>
2024-02-24 15:38:59 +0330

    Update mtproto-config.c

--- mtproto/mtproto-config.c
+++ mtproto/mtproto-config.c
@@ -1,27 +1,3 @@
-/*
-    This file is part of MTProto-Server
-
-    MTProto-Server is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
-
-    MTProto-Server is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MTProto-Server.  If not, see <http://www.gnu.org/licenses/>.
-
-    This program is released under the GPL with the additional exemption
-    that compiling, linking, and/or using OpenSSL is allowed.
-    You are free to remove this exemption from derived works.
-
-    Copyright 2012-2018 Nikolai Durov
-              2012-2014 Andrey Lopatin
-              2014-2018 Telegram Messenger Inc
-*/
 #define        _FILE_OFFSET_BITS        64
 
 #include <assert.h>
