dd76395201eac0129af1100aaba72e4c543c2f22
aphrco <52441136+aphrco@users.noreply.github.com>
2024-02-24 15:39:30 +0330

    Update sha256.h

--- common/sha256.h
+++ common/sha256.h
@@ -1,22 +1,3 @@
-/*
-    This file is part of KittenDB/Engine Library.
-
-    KittenDB/Engine Library is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
-
-    KittenDB/Engine Library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with KittenDB/Engine Library.  If not, see <http://www.gnu.org/licenses/>.
-
-    Copyright 2016 Telegram Messenger Inc
-              2016 Nikolai Durov
-*/
 
 #pragma once
 
